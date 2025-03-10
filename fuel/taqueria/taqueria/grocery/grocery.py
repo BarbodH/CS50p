a9e76a6a4d6ff44da2ae5db83fffe09ad0b22846
BarbodH <barbod.hasanzadeh01@gmail.com>
2025-03-10 20:28:20 +0000

    Mon, Mar 10, 2025, 8:28 PM +00:00

--- fuel/taqueria/taqueria/grocery/grocery.py
+++ fuel/taqueria/taqueria/grocery/grocery.py
@@ -3,7 +3,16 @@ secondList = []
 
 while True:
     try:
-        grocery = int(input())
+        grocery = input()
         firstList.append(grocery)
-        
+        for item in firstList:
+            if item not in secondList:
+                secondList.append(item)
+
+        for i in secondList:
+            i.upper()
+
+    except EOFError:
+        print(i)
+        break
 
