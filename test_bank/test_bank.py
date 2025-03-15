256bc0afe5175095264a23ed54b1d050a61fd74e
BarbodH <barbod.hasanzadeh01@gmail.com>
2025-03-15 18:17:14 +0000

    Sat, Mar 15, 2025, 6:17 PM +00:00

--- test_bank/test_bank.py
+++ test_bank/test_bank.py
@@ -1,7 +1,10 @@
 from bank import value
+import pytest
 
 def test_value():
     assert ("Hello") == "$0"
     assert ("hello") == "$0"
     assert ("What's up?") == "$100"
-    assert
+    assert ("how good you are!") == "$20"
+
+
